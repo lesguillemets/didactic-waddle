4824b8482110ed74f20887a31ea8f4287eb9701b
lesguillemets <lesguillemets@users.noreply.github.com>
2016-03-25 01:04:50 +0900

    wip

--- src/didactic_waddle/core.cljs
+++ src/didactic_waddle/core.cljs
@@ -1,39 +1,56 @@
 (ns didactic-waddle.core
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]))
+(defn make-box [x y base-size]
+  {:color 0,
+   :size  1,
+   :growing true,
+   :angle 0,
+   :x x, :y y,
+   :base-size base-size})
+
+(defn next-stage [box]
+  (let [size (:size box)
+        growing (:growing box)]
+    (assoc box
+           :color (mod (+ (:color box) 0.7) 255),
+           :angle (mod (+ (:angle box) 0.02) (* 2 Math/PI)),
+           :size (+ size
+                    (if growing 0.01 -0.01)),
+           :growing (if growing
+                      (if (< 1.5 size) false true)
+                      (if (< size 0.5) true false)))))
+
+(defn update-state [s] {:boxes (map next-stage (:boxes s))})
+
 
 (defn setup []
   (q/frame-rate 60)
   ; Set color mode to HSB (HSV) instead of default RGB.
   (q/color-mode :hsb)
-  {:color 0,
-   :size  1,
-   :growing true,
-   :angle 0
-   })
+  {:boxes (map
+            (fn [n]
+              (nth (iterate next-stage
+                            (make-box (* 30 (mod n 5))
+                                      (* 30 (quot n 5))
+                                      20))
+                   n))
 
-(defn update-state [s]
-  (let [size (:size s)
-        growing (:growing s)]
-    {:color (mod (+ (:color s) 0.7) 255),
-     :angle (mod (+ (:angle s) 0.02) (* 2 Math/PI)),
-     :size (+ size
-              (if growing 0.01 -0.01)),
-     :growing (if growing
-                (if (< 1.5 size) false true)
-                (if (< size 0.5) true false))}))
+            (range 25))})
+
+(defn draw-box [box]
+  (q/stroke (:color box) 255 255)
+  (let [size (* (:base-size box) (:size box))]
+    (q/rect-mode :center)
+    (q/with-translation [(:x box) (:y box)]
+      (q/with-rotation [(:angle box)]
+        (q/rect 0 0 size size)))))
 
 (defn draw-state [state]
   ; Clear the sketch by filling it with light-grey color.
   (q/background 240)
   ; Set circle color.
-  (q/stroke (:color state) 255 255)
-  (let [size (* 200 (:size state))
-        centre (/ (q/width) 2)]
-    (q/rect-mode :center)
-    (q/with-translation [centre centre]
-      (q/with-rotation [(:angle state)]
-        (q/rect 0 0 size size)))))
+  (map draw-box (:boxes state)))
 
 (q/defsketch didactic-waddle
   :host "didactic-waddle"
