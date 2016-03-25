357ece714e5e51bd24bbe51f8592b5ab16068c10
lesguillemets <lesguillemets@users.noreply.github.com>
2016-03-26 01:21:47 +0900

    it's ... moving . . .!

--- src/didactic_waddle/sketch01.cljs
+++ src/didactic_waddle/sketch01.cljs
@@ -2,7 +2,13 @@
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]))
 
-(def update-state identity)
+(defn update-state [s]
+  (map (fn [c]
+         (assoc
+           c
+           :x (+ 1 (:x c)),
+           :y (+ 1 (:y c))
+           )) s))
 (def size 500)
 (def dist 10)
 (def TwoPI (* Math/PI 2))
@@ -10,20 +16,23 @@
 (defn setup []
   (q/frame-rate 60)
   (q/color-mode :hsb)
-  (q/no-loop)
-  (map (fn [n] {:x (* dist (mod n 51)),
-                :y (* dist (quot n 51))})
+  ; (q/no-loop)
+  (map (fn [n] {:x (mod n 51),
+                :y (quot n 51),
+                :n n})
        (range 2750)))
 
 (defn draw-circle [circ]
   (q/no-stroke)
   (let [x (:x circ)
         y (:y circ)
-        noise (q/noise (/ x 300) (/ y 300))
+        noise (q/noise (/ x 30) (/ y 30))
+        x-loc (* dist (mod (:n circ) 51))
+        y-loc (* dist (quot (:n circ) 51))
         radius (* (Math/sqrt noise) dist 1.2)
         ]
     (q/fill (* 255 noise) 200 200)
-    (q/arc x y radius radius 0 TwoPI)))
+    (q/arc x-loc y-loc radius radius 0 TwoPI)))
 
 (defn draw-state [state]
   (q/background 250)
