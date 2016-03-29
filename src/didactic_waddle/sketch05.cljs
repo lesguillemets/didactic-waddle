53ea8e137ec4a38f5ab90e2ea460175bc7825a45
lesguillemets <lesguillemets@users.noreply.github.com>
2016-03-29 15:32:45 +0900

    game of life

--- src/didactic_waddle/sketch05.cljs
+++ src/didactic_waddle/sketch05.cljs
@@ -2,33 +2,66 @@
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]))
 
-(def width 250)
-(def height 250)
+(def width 100)
+(def height 100)
 (def hue 100)
 (def sat 200)
 (def max-b 200)
 (def area (* width height))
 (def sq (fn [n] (* n n)))
 
-(defn update-state [s] s)
+(defn random-field [w h]
+  (vec
+    (for [y (range h)]
+      (vec
+        (for [x (range w)]
+          (rand-int 2))))))
+
+(defn neighbouring [x y]
+  (for [dx (range -1 2)
+        dy (range -1 2)
+        :when (not (and (= dx 0) (= dy 0)))]
+    [(+ x dx) (+ y dy)]))
+
+(defn get-neighbours [field x y]
+  (map (fn [[x y]] (get-in field [y x] 0))
+       (neighbouring x y)))
+
+(defn check-spawn [v neighbours]
+  (if (= v 1)
+    (cond
+      (< neighbours 2) 0
+      (< neighbours 4) 1
+      :else 0)
+    (cond
+      (= neighbours 3) 1
+      :else 0)))
+
+
+(defn next-field [field]
+  (vec
+    (for [y (range height)]
+      (vec
+        (for [x (range width)]
+          (let [v (apply + (get-neighbours field x y))]
+            (check-spawn (get-in field [y x] 0) v)))))))
+
+(defn update-state [s]
+  (assoc s :field (next-field (:field s))))
+
 (defn setup []
   (q/color-mode :hsb)
-  (q/frame-rate 30)
+  (q/frame-rate 2)
   (q/background 0)
-  (q/no-loop)
-  {:field
-   (vec
-     (for [y (range height)]
-       (vec
-         (for [x (range width)]
-           (rand)))))
-   })
+  (enable-console-print!)
+  ; (q/no-loop)
+  {:field (random-field width height)})
 
 (defn to-color [value]
   [hue sat (* max-b value)])
 
-(defn draw-dot-at [x y field]
-  (let [color (to-color (nth (nth field y) x))]
+(defn draw-dot-at [field x y]
+  (let [color (to-color (get-in field [y x]))]
     (apply q/stroke color) ; with-stroke doesn't work somehow
     (q/point x y)
     ))
@@ -37,7 +70,7 @@
   (q/background 0)
   (doseq [y (range height)]
     (doseq [x (range width)]
-      (draw-dot-at x y (:field s))))
+      (draw-dot-at (:field s) x y)))
   )
 
 
